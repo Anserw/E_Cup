3c6bb3aff39a0664ecc60d9332c3f2267b1ebb64
Anserw <anserw.x@gmail.com>
2016-06-11 04:56:25 +0800

    update

--- README.md
+++ README.md
@@ -1,51 +1,39 @@
-## E cup guess competition report
-### real-time ranking
-time: 2016-04-27 16:25:17
+# E cup guess competition report
+## real-time ranking
+2016-06-11 04:55:21
 
 |rank|name|score|
 |:---:|:---:|:---:|
-|1|laotao|11.55|
-|2|laoda|4.7|
-|3|mother|-0.1|
-|4|wan|-1.05|
-|5|lc|-1.05|
-|6|dazuan|-1.05|
-|7|xiaobai|-3|
-|8|laopai|-3|
+|1|laopai|6.33|
+|2|laoda|3.17|
+|3|mother|3.17|
+|4|laotao|-1.33|
+|5|wan|-2.00|
+|6|lc|-2.00|
+|7|xiaobai|-2.00|
+|8|dazuan|-2.00|
 
-### Pond
-93.0
+## Pond
+615.67
 
-### History
+## History
 
-#### - 20160501 France 3 : 1 England
-handicap: -1 / -1.5
-scorer: laotao
+### 20160501 France 2 : 1 Romania
+- handicap: -1.5
+- scorer: laotao
+- owners:
+ - France: laotao
+ - Romania: laopai
 
-|name|guess|score change|
-|:---:|:---:|:---:|
-|wan|England|-2|
-|lc|England|-2|
-|laoda|France|5.7|
-|xiaobai|England|-2|
-|mother|England|-2|
-|laopai|England|-2|
-|dazuan|England|-2|
-|laotao|France|12.3|
-|Pond|..|-6.0|
-
-#### - 20160501 France 2 : 1 England
-handicap: -1 / -1.5
-scorer: laotao
 
 |name|guess|score change|
 |:---:|:---:|:---:|
-|wan|England|0.95|
-|lc|England|0.95|
-|laoda|France|-1|
-|xiaobai|France|-1|
-|mother|England|1.9|
-|laopai|France|-1|
-|dazuan|England|0.95|
-|laotao|France|-0.75|
-|Pond|..|-1.0|
+|wan|France|-2.00|
+|lc|France|-2.00|
+|laoda|Romania|3.17|
+|xiaobai|France|-2.00|
+|mother|Romania|3.17|
+|laopai|Romania|6.33|
+|dazuan|France|-2.00|
+|laotao|France|-1.33|
+|Pond|..|-3.33|
--- run.py
+++ run.py
@@ -3,30 +3,39 @@ import core
 import time
 
 def generate_markdown(players, pond, matches, teams, bets, filename="README.md"):
-    md = u'''## E cup guess competition report
-### real-time ranking
+    md = u'''# E cup guess competition report
+## real-time ranking
 '''
     ISOTIMEFORMAT='%Y-%m-%d %X'
-    md += "time: " + time.strftime( ISOTIMEFORMAT, time.localtime()) + '\n'
+    md += time.strftime( ISOTIMEFORMAT, time.localtime()) + '\n'
 
     md += "\n|rank|name|score|\n|:---:|:---:|:---:|\n"
     rank = 0
     for player_name in sorted(players, key=lambda p: players[p], reverse=True):
         rank += 1
-        md += '|' + str(rank) + '|' + player_name + '|' + str(players[player_name]) + '|\n'
+        md += '|' + str(rank) + '|' + player_name + '|' + "%.2f" % players[player_name] + '|\n'
 
-    md += "\n### Pond\n" + str(pond["sum"]) + '\n'
+    md += "\n## Pond\n" + "%.2f" % pond["sum"] + '\n'
 
-    md += "\n### History\n"
+    md += "\n## History\n"
     for a_match in sorted(matches, cmp=lambda a, b: a["ID"] < b["ID"]):
-        md += "\n#### - " + str(a_match["date"]) + ' ' + a_match["teamA"] + ' ' + str(a_match["scoreA"]) + \
+        md += "\n### " + str(a_match["date"]) + ' ' + a_match["teamA"] + ' ' + str(a_match["scoreA"]) + \
               " : " + str(a_match["scoreB"]) + ' ' + a_match["teamB"] + '\n'
 
         if a_match["HandicapA"] == a_match["HandicapB"]:
-            md += "handicap: " + str(a_match["HandicapA"]) + '\n'
+            md += "- handicap: " + str(a_match["HandicapA"]) + '\n'
         else:
-            md += "handicap: " + str(a_match["HandicapA"]) + ' / ' + str(a_match["HandicapB"]) + '\n'
-        md += "scorer: " + a_match["scorer"] + '\n'
+            md += "- handicap: " + str(a_match["HandicapA"]) + ' / ' + str(a_match["HandicapB"]) + '\n'
+        md += "- scorer: " + a_match["scorer"] + '\n'
+        md += "- owners:\n"
+        md += " - " + a_match["teamA"] + ": "
+        for a_team in teams:
+            if a_team == a_match["teamA"]:
+                md += teams[a_team] + "\n"
+        md += " - " + a_match["teamB"] + ": "
+        for a_team in teams:
+            if a_team == a_match["teamB"]:
+                md += teams[a_team] + "\n\n"
         md += "\n|name|guess|score change|\n|:---:|:---:|:---:|\n"
         report = dict()
         pond_report = {"sum": 0}
@@ -34,10 +43,8 @@ def generate_markdown(players, pond, matches, teams, bets, filename="README.md")
             report[player_name] = 0
         core.process(report, teams, a_match, bets[a_match["ID"]], pond_report)
         for player_name in report:
-            md += '|' + player_name + '|' + bets[a_match["ID"]][player_name] + '|' + str(report[player_name]) + '|\n'
-        md += '|' + "Pond" + '|' + '..' + '|' + str(pond_report["sum"]) + '|\n'
-
-
+            md += '|' + player_name + '|' + bets[a_match["ID"]][player_name] + '|' + "%.2f" % report[player_name] + '|\n'
+        md += '|' + "Pond" + '|' + '..' + '|' + "%.2f" % pond_report["sum"] + '|\n'
 
     f = open(filename, 'w')
     f.write(md)
@@ -50,17 +57,17 @@ if __name__ == "__main__":
     matches = loader.loadMatch()
     bets = loader.loadBet()
     pond = loader.loadPond()
-    reports = dict()
+    # reports = dict()
     for a_match in matches:
         core.process(players, teams, a_match, bets[a_match["ID"]], pond)
-        report = dict()
-        pond_report = {"sum": 0}
-        for player_name in players:
-            report[player_name] = 0
-        core.process(report, teams, a_match, bets[a_match["ID"]], pond_report)
-        report["ID"] = a_match["ID"]
-        report["pond"] = pond_report["sum"]
-        reports["ID"] = report
-    print "result", players
-    print "report", report
+        # report = dict()
+        # pond_report = {"sum": 0}
+        # for player_name in players:
+        #     report[player_name] = 0
+        # core.process(report, teams, a_match, bets[a_match["ID"]], pond_report)
+        # report["ID"] = a_match["ID"]
+        # report["pond"] = pond_report["sum"]
+        # reports["ID"] = report
+    # print "result", players
+    # print "report", report
     generate_markdown(players, pond, matches, teams, bets)
